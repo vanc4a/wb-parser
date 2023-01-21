45af9091eaef1d516cb09fb8fb5dfcbde31de2f0
vanc4a <34973234+vanc4a@users.noreply.github.com>
2023-01-21 20:31:54 +0300

    request query fix

--- src/Controller.js
+++ src/Controller.js
@@ -3,11 +3,9 @@ const DataProvider = require("./providers/DataProvider")
 exports.promo = (req,res) => {
     let dataProvider = new DataProvider()
     let searchString = ''
-
     if(!req.query.text){
         return res.status(404).send('Nothing to search.')
     }
-    searchString = req.query.text.replace('%',' ')
     dataProvider.getPromo(searchString).then(promo => res.send(promo)).catch(err => {
         if(err == 404) {
             return res.status(404).send('Bad request.')
